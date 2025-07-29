8284f334d1c5c886c460c58f31e7a4750c0eaa9f
Sardor20082 <xfiles20242008@gmail.com>
2025-07-29 15:12:05 +0500

    Create languages.py

---
+++ languages.py
@@ -0,0 +1,28 @@
+LANGS = {
+    "uz": {
+        "welcome": "Assalomu alaykum! Quyidagi ijtimoiy tarmoqni tanlang:",
+        "choose_quality": "Videoning sifatini tanlang:",
+        "send_link": "Iltimos, {} video havolasini yuboring.",
+        "not_subscribed": "Botdan foydalanish uchun quyidagi kanalga obuna bo‘ling:",
+        "stat": "Bot foydalanuvchilari soni: {} ta",
+        "broadcast_prompt": "Yubormoqchi bo‘lgan xabaringizni yozing:"
+    },
+    "ru": {
+        "welcome": "Здравствуйте! Выберите социальную сеть ниже:",
+        "choose_quality": "Выберите качество видео:",
+        "send_link": "Пожалуйста, отправьте ссылку на видео с {}.",
+        "not_subscribed": "Чтобы использовать бота, подпишитесь на канал:",
+        "stat": "Количество пользователей бота: {}",
+        "broadcast_prompt": "Введите сообщение для рассылки:"
+    },
+    "en": {
+        "welcome": "Welcome! Please choose a social network:",
+        "choose_quality": "Choose video quality:",
+        "send_link": "Please send a video link from {}.",
+        "not_subscribed": "To use this bot, please subscribe to the channel:",
+        "stat": "Total bot users: {}",
+        "broadcast_prompt": "Type the message to broadcast:"
+    }
+}
+
+USER_LANG = {}
